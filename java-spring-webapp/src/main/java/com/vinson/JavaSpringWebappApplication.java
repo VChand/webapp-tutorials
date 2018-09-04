136e5390ba322f6b3a1eabc44633bc00fd0eed89
vinsonchand <vinsonchand@192.168.0.8>
2018-09-03 21:45:47 -0400

    Initial Commit

---
+++ java-spring-webapp/src/main/java/com/vinson/JavaSpringWebappApplication.java
@@ -0,0 +1,12 @@
+package com.vinson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringWebappApplication.class, args);
+	}
+}
